a090c541f93f64fff6a2fbd59419ecca85e36559
huydsai02 <huydsai02@gmail.com>
2025-11-04 19:04:46 +0700

    Update config

--- fe/client/vite.config.js
+++ fe/client/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname),
   server: {
-    port: 3000
+    port: 3000,
   }
 })
